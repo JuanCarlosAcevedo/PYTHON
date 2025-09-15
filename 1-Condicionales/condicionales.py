8b847d9cf6ce52cef2b197f09c144899eed14aa2
Juan Chano Acevedo <jacevedomotta@frba.utn.edu.ar>
2025-09-15 19:00:23 -0300

    Update condicionales.py

--- 1-Condicionales/condicionales.py
+++ 1-Condicionales/condicionales.py
@@ -1,3 +1,7 @@
+# Se solicita al usuario que ingrese su nombre, apellido, edad y email.
+# Si no hay espacios en blanco en nombre, apellido, email y la edad es mayor a 18, se imprime en pantalla los datos
+# Si los datos estan en blancos o la edad es menor a 18, se imprime ERROR!
+
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
 edad = int(input("Ingrese su edad: "))
